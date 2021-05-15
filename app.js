b19e1e48f5cc0593108ea36f2ddb45429839ac33
Mrigank <msachan562000@gmail.com>
2021-05-15 18:33:21 +0530

    added get routes

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const _ = require("lodash");
 const searchtext = "";
 const request=require("request");
 
-// var passport=require("passport"),
-//     User=require("./models/user"),
-//     localstrategy=require("passport-local"),
-//     passportlocalmongoose=require("passport-local-mongoose");
+
 
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -34,21 +31,33 @@ app.get('/', function(req, res) {
 app.get('/signin', function(req, res) {
   res.render('signin');
 });
+
+app.post("/ticket_booking",function(req,res){
+  res.render("ticket_booking")
+});
+
 app.get('/Seat_Availability', function(req, res) {
   res.render('Seat_Availability');
 });
+
+
 app.get('/contact', function(req, res) {
   res.render('contact');
 });
+
+
 app.get('/ticket_schedule', function(req, res) {
   res.render('ticket_schedule');
 });
+
 
 app.get('/signup1', function(req, res) {
   res.render('signup1');
 });
 
-//add the router
+
+
+
 
 app.listen(process.env.port || 3000);
 
